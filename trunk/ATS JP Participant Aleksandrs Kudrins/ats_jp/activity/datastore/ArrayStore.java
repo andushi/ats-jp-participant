a060aceaf5a264ef3a6bd3044b3f4ee3155ef5cf
studentBC <studentBC@c75f3f90-c936-0410-bf52-159432426859>
2007-08-07 06:45:16 +0000

    Initial import.

---
+++ trunk/ATS JP Participant Aleksandrs Kudrins/ats_jp/activity/datastore/ArrayStore.java
@@ -0,0 +1,30 @@
+//TODO Activity 4.x
+//Implement this class.  Refer to the participant guide for the instructions
+
+/*
+ * Created on Aug 11, 2005
+ * Accenture Manila Delivery Center
+ * Technology Managed Services
+ * Makati Stock Exchange Bldg
+ * Makati City, Philippines
+ */
+//
+package ats_jp.activity.datastore;
+
+public class ArrayStore extends AbstractArrayStore {
+    
+    //You have a free hand in implementing the requirements of this class.  You may declare any
+    //additional members that you deem necessary.  The only restriction again is that you do not
+    //violate the encapsulation of the class.
+    
+    //HINT: This class has direct access to the protected and public members of AbstractArrayStore
+    
+    public ArrayStore(int size) { 
+        super(size);
+    }
+
+    public ArrayStore(){
+        super();
+    }
+  
+}
